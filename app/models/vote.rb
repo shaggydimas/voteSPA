7acc25f557ed9cca08cd860111ddb047a2d3b80f
zollex <dmitri@bind.ee>
2017-05-21 20:46:18 +0000

    heroku test

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,5 +1,5 @@
 class Vote < ActiveRecord::Base
-    #before_save :save_as_json
+    before_save :save_as_json
     validates :title, presence: true, length: { minimum: 5, maximum: 70 }
     validates :options, presence: true
     belongs_to :user
